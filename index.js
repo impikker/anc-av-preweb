5406265db8995260d98c4bcc7000d2d6c2a57102
pookpik <missphanu@gmail.com>
2024-08-11 19:25:13 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,94 @@
+var News = [
+    { id: 1, img: '11.jpg', detail: 'สำนักงานคณะกรรมการกำกับและส่งเสริมการประกอบธุรกิจประกันภัย ประชาสัมพันธ์และให้ความรู้ ', date: '11 กรกฎาคม 2567 เวลา 08.50 น.' },
+    { id: 2, img: '12.jpg', detail: 'วันที่ 12 กรกฎาคม 2567 โรงเรียนอำนาจเจริญ ร่วมกับ มลฑลทหารบกที่ 22', date: '11 กรกฎาคม 2567 เวลา 08.50 น.' },
+    { id: 3, img: '13.jpg', detail: 'นักเรียนห้องเรียนพิเศษคอมพิวเตอร์ ICTP ANC ได้เข้าร่วมกิจกรรมในโครงการ Coding for Better Life', date: '11 กรกฎาคม 2567 เวลา 08.50 น.' },
+    { id: 4, img: '14.jpg', detail: 'โรงเรียนอำนาจเจริญ นำนักเรียนร่วมแสดง เพื่อเป็นตัวเเทนจังหวัดในการประกวดการเเสดงบนเวที', date: '11 กรกฎาคม 2567 เวลา 08.50 น.' },
+    { id: 5, img: '15.jpg', detail: 'โรงเรียนอำนาจเจริญได้เปิดรับสมัครนักเรียนชั้นมัธยมศึกษาปีที่ 4 ในปีการศึกษา 2567', date: '11 กรกฎาคม 2567 เวลา 08.50 น.' },
+    { id: 6, img: '16.jpg', detail: 'ขอแสดงความยินดีให้กับนักเรียนที่ทำความดี', date: '11 กรกฎาคม 2567 เวลา 08.50 น.' },
+    { id: 7, img: '17.jpg', detail: 'ขอแสดงความยินดีให้กับนักเรียนโรงเรียนอำนาจเจริญที่ผ่านการเข้ารอบ ค่ายสอวน. ค่าย 1', date: '11 กรกฎาคม 2567 เวลา 08.50 น.' },
+    { id: 8, img: '18.jpg', detail: 'วันพระใหญ่ทางโรงเรียนอำนาจเจริญได้นำนักเรียนและคณะครูอาจารย์ เข้าร่วมรำถวายพระมงคลมิ่งเมือง', date: '11 กรกฎาคม 2567 เวลา 08.50 น.' },
+    { id: 9, img: '19.jpg', detail: 'โรงเรียนอำนาจเจริญ ได้จัดกิจกรรมเครื่องแบบ เข้าค่ายลูกเสือ เนตรนารี และยุวกาชาด', date: '11 กรกฎาคม 2567 เวลา 08.50 น.' }
+];
+
+var fulldetail = [
+    {
+        newsdetail:'วันที่ 11 กรกฏาคม 2567 สำนักงานคณะกรรมการกำกับและส่งเสริมการประกอบธุรกิจประกันภัย ประชาสัมพันธ์และให้ความรู้ สืบเนื่องจากโครงการอัจฉริยะยุวชนประกันภัย ประจำปี 2567 ณ กิจกรรมหน้าเสาธง โรงเรียนอำนาจเจริญ'
+    },
+    {
+        newsdetail:'วันที่ 12 กรกฎาคม 2567 โรงเรียนอำนาจเจริญ ร่วมกับ มลฑลทหารบกที่ 22 เป็นตัวแทนจังหวัดอำนาจเจริญ เข้าร่วมการแข่งขันประกวดดนตรีสากลร่วมสมัย คีตวัฒนศิลป์ บรรเลงเพลงไทยเพื่อแผ่นดิน ปีที่ 3 ประจำปี 2567 โดยมี นายสุกิจ จันทบาล ผู้อำนวยการโรงเรียนอำนาจเจริญ เป็นประธานในพิธีเปิดการแข่งขัน ภายใต้แนวคิด ปลูกกล้า บรรเลงรักษ์ แด่องค์ราชัน ณ หอประชุมศรีอำนาจเจริญ'
+    },
+    {
+        newsdetail:'นักเรียนห้องเรียนพิเศษคอมพิวเตอร์ ICTP ANC  ได้เข้าร่วมกิจกรรมในโครงการ Coding for Better Life สร้างรากฐานอนาคตประเทศไทย11 กรกฎาคม 2567, จังหวัดอุบลราชธานี - สำนักงานส่งเสริมเศรษฐกิจดิจิทัล (ดีป้า) จัดกิจกรรม Coding Bootcamp & Roadshow และ Coding War พื้นที่ภาคอีสานตอนล่าง ภายใต้โครงการ Coding for Better Life สร้างรากฐานอนาคตประเทศไทย หลังกิจกรรมในพื้นที่ภาคอีสานตอนกลางและภาคกลางได้รับกระแสตอบรับดีเยี่ยม รุกติวเข้มทักษะโค้ดดิ้งนักเรียน – ครู สร้างความตระหนักรู้ประชาชน เรียนรู้ทักษะโค้ดดิ้ง และอัปเดตเทรนด์เทคโนโลยี นวัตกรรม และ AI พร้อมเปิดพื้นที่ให้เด็กไทยแข่งขันทักษะด้านโค้ดดิ้งเพื่อเข้าร่วมแข่งขัน Coding War รอบ Final  ที่กรุงเทพฯ ระหว่างวันที่ 13 - 15 กันยายนนี้ ชี้โครงการดังกล่าวจะช่วยสร้างกำลังคนดิจิทัลที่มีศักยภาพรองรับ ความต้องการของภาคอุตสาหกรรม และแก้ไขปัญหาการขาดแคลนกำลังคนดิจิทัลของประเทศได้อย่างยั่งยืนทีม ICTP ANC นายทสพร เสมอดี    ชั้นม. 5/6นางสาวธัญชนก ภูมิกาล ชั้นม. 5/6นางสาวอักษราภัค สหารัตน์ ชั้นม.4/6ฝึกสอนและควบคุมทีมโดย ครูอารีรัตน์ ธานี อำนวยการโดย นายสุกิจ  จันทบาล  ผู้อำนวยการโรงเรียนอำนาจเจริญ ได้รับรางวัลรองชนะเลิศอันดับ 3 และได้ร่วมเป็นตัวแทน ภาคอีสานตอนล่างเข้าร่วมแข่งขัน รอบไฟนอล ต่อไป'
+    },
+    {
+        newsdetail:'โรงเรียนอำนาจเจริญ นำนักเรียนร่วมแสดง เพื่อเป็นตัวเเทนจังหวัดในการประกวดการเเสดงบนเวที ประเภทจังหวัด TO BE NUMBER ONE ระดับประเทศ ประจำปี 2567 ระหว่างวันที่ 12-16 กรกฎาคม 2567 ณ ศูนย์การประชุมอิมแพ็ค ฟอรั่ม เมืองทองธานี จังหวัดนนทบุรี ฝึกซ้อมและควบคุมการแสดงโดย คุณครูปฎิมาพร พูลศิริ และคุณครูโสริยา พูลศิริ'
+    },
+    {
+        newsdetail:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        newsdetail:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        newsdetail:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        newsdetail:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+    {
+        newsdetail:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    },
+]
+
+
+$(document).ready(() => {
+    var html = '';
+    for (let i = 0; i < News.length; i++) {
+        html += `<div class="content" onclick="openNewsDetail(${i})">
+                    <img class="img-set" src="${News[i].img}" alt="News Image">
+                    <p>${News[i].detail}</p>
+                </div>`;
+    }
+    $("#newslist").html(html);
+    $(".modal-bg").on("click", function() {
+        closeModal();
+    });
+});
+
+$(document).ready(() => {
+    var html = '';
+    for (let i = 0; i < News.length; i++) {
+        html += `<div class="content" onclick="openNewsDetail(${i})">
+                    <img class="img-set" src="${News[i].img}" alt="News Image">
+                    <p>${News[i].detail}</p>
+                </div>`;
+    }
+    $("#newslist-all").html(html);
+    $(".modal-bg").on("click", function() {
+        closeModal();
+    });
+});
+
+var newsindex = 0;
+function openNewsDetail(index) {
+    newsindex = index;
+    console.log(newsindex);
+    const newsItem = News[newsindex];
+    $(".modal-img").attr("src", newsItem.img);
+    $(".modal-detail p").text(fulldetail[newsindex].newsdetail); // Use fulldetail here
+    $(".date-news").text(newsItem.date);
+    $("#modalDesc").css('display', 'flex');
+}
+
+function closeModal() {
+    $("#modalDesc").css('display', 'none');
+}
+
+function downloadFile(url) {
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = url.split('/').pop();  // ชื่อไฟล์อาจถูกตั้งตาม URL
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
